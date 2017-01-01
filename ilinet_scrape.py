8417b7b74b66e58222716908953e241623973104
BerniceBoursiquot <boursiquot@gmail.com>
2017-01-01 17:14:31 -0500

    Importing libraries

--- ilinet_scrape.py
+++ ilinet_scrape.py
@@ -1,6 +1,8 @@
-#import requests 
-import urllib2 as url
+#import requests
+ import urllib2 as url
+
 from bs4 import BeautifulSoup as bs
+from lxml import html 
 
 site = "https://www.cdc.gov/flu/weekly/weeklyarchives2016-2017/data/senAllregt50.html"
 
@@ -8,14 +10,18 @@ page = url.urlopen(site)
 
 soup = bs(page,"html.parser")
 
-#print soup.prettify()
-
+right_table  = soup.find_all("table")
+#print all_tables #print soup.prettify() 
 #print soup.title.string
 
-table_headers = soup.find_all("th")
+table_headers = soup.findAll("tr",limit = 2)
 
-print type(table_headers)
+print table_headers 
 
-#ili_data = soup.find_all("td")
+ili_data = soup.find_all("td")
+    #for row in right_table.findAll("tr"):
+#    cell = row.findAll("td")
+#    headers = row.findAll("th") 
+    
 
-#print ili_data
+    
