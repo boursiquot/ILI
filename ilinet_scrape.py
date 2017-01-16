648ea6a07521499b7389338dc89578b90de82b4d
BerniceBoursiquot <boursiquot@gmail.com>
2017-01-16 14:12:02 -0500

    Added “Region Type”, “Region”, and “Year” columns; split “Week” column into “Year” and “Week”; updated site object

--- ilinet_scrape.py
+++ ilinet_scrape.py
@@ -2,9 +2,9 @@
 import urllib2 as url
 import pandas as pd
 from bs4 import BeautifulSoup as bs
-from lxml import html 
+from lxml import html
 
-site = "https://www.cdc.gov/flu/weekly/weeklyarchives2016-2017/data/senAllregt50.html"
+site = "https://www.cdc.gov/flu/weekly/weeklyarchives2016-2017/data/senAllregt01.html"
 
 page = url.urlopen(site)
 
@@ -12,28 +12,37 @@ soup = bs(page,"html.parser")
 
 table_headers = soup.findAll("tr",limit = 2)[0].findAll("th")
 
-#print table_headers 
-
 column_headers = [th.getText() for th in soup.findAll("tr",limit = 2)[0].findAll("th")]
 
-#print column_headers,type(column_headers)
+column_headers.insert(0,"Region Type")
+
+column_headers.insert(1,"Region")
+
+column_headers.insert(2,"Year")
+
+print column_headers
 
 data_rows = soup.findAll("tr")[1:]
 
-#print data_rows,type(data_rows)
-
 ili_data = [[td.getText() for td in data_rows[i].findAll("td")] for i in range(len(data_rows))]
 
-ili_data2  = pd.to_numeric(ili_data) 
-print ili_data2
+for i in ili_data:
+    x = str(i[0])
+    i.pop(0) 
+    year = x[0:4]
+    week = x[4:]
+    i.insert(0,year)
+    i.insert(1,week)
+    i.insert(0,"National")
+    i.insert(1,"X")
 
-#df = pd.DataFrame(ili_data, columns = column_headers)
+print ili_data    
 
-#df = df.convert_objects(convert_numeric = True)
+df  = pd.DataFrame(ili_data, columns = column_headers)
 
-#df = pd.to_numeric(df)
+#df = df.apply(pd.to_numeric)
 
-#print df.dtypes
+print df
 
 #df.loc[:,"Week"] = df.loc[:,"Week"].astype(str)
 #print  df
